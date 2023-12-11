317630cbeda0e892e1a4505e78e95f26e1eeaa2d
j-mowery <98827382+j-mowery@users.noreply.github.com>
2023-12-11 15:30:12 -0500

    BATIAI-1936 - Update Default Bucket Names (#6)
    * BATIAI-1936 - merge current default name as override
    
    * Fixing bucket prop merge
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Charles Bushong <charles.bushong@cms.hhs.gov>
    Co-authored-by: Charles Bushong <cbushong@fearless.tech>

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ locals {
     lifecycle_expiration_days = null
   }
   default_buckets = {
-    loki   = local.default_parameters
-    thanos = local.default_parameters
-    velero = local.default_parameters
+    loki   = merge(local.default_parameters, {bucket_name_override = "${var.cluster_name}-logs"})
+    thanos = merge(local.default_parameters, {bucket_name_override = "batcave-${var.cluster_name}-thanos"})
+    velero = merge(local.default_parameters, {bucket_name_override = "${var.cluster_name}-batcave-velero-storage"})
   }
-  all_buckets = merge(local.default_buckets, var.bucket_specs)
+  all_bucket_names = toset(concat(keys(local.default_buckets), keys(var.bucket_specs)))
+  all_buckets = {for name in local.all_bucket_names : name => merge(try(local.default_buckets[name], {}), try(var.bucket_specs[name], {}))}
 }
 
 module "bucket" {
